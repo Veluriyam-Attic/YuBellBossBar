48ae25ed32c100d04e86576b271407332bf4876a
Henceforth <3412615165@qq.com>
2026-03-30 23:43:16 +0800

    Holy Shit this is a big work

--- Content/BarData.cs
+++ Content/BarData.cs
@@ -16,7 +16,7 @@ internal static class BarData
 
     public static void InstantiateBuildInContent()
     {
-        #region Default Texture
+        #region 默认贴图 Default Texture
         #region 金色风格 Gloden Style
         buildiincontent.Add(int.MaxValue,
             new BarInfo(
@@ -96,11 +96,158 @@ internal static class BarData
         #endregion
         #endregion
 
-        #region Default Vanilla
+        #region 默认原版 Default Vanilla
+        #region 血肉墙 Wall of Flesh
+        buildiincontent.Add(NPCID.WallofFlesh,
+            new BarInfo(
+                new BarTextures(
+                    NPCID.WallofFlesh,
+                    new Dictionary<TextureType, BarTexture2D>()
+                    {
+                        {
+                            TextureType.Frame,
+                            new BarTexture2D(TextureType.Frame,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshFrame",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla,
+                                (barFillSytle,barFillColot,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; }
+                            )
+                        },
+                        {
+                            TextureType.Head,
+                            new BarTexture2D(
+                                TextureType.Head,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshHead",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                        {
+                            TextureType.Tail,
+                            new BarTexture2D(
+                                TextureType.Tail,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshTail",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                    }
+                )
+            )
+        );
+        #endregion
+        #region 血肉墙眼 Wall of Flesh Eye
+        buildiincontent.Add(NPCID.WallofFleshEye,
+            new BarInfo(
+                new BarTextures(
+                    NPCID.WallofFleshEye,
+                    new Dictionary<TextureType, BarTexture2D>()
+                    {
+                        {
+                            TextureType.Frame,
+                            new BarTexture2D(TextureType.Frame,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshFrame",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla,
+                                (barFillSytle,barFillColot,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; }
+                            )
+                        },
+                        {
+                             TextureType.Head,
+                             new BarTexture2D(TextureType.Head,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshHead",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                             )
+                        },
+                        {
+                             TextureType.Tail,
+                             new BarTexture2D(TextureType.Tail,ModContent.Request<Texture2D>
+                                (_dvpath + "WallofFleshTail",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                             )
+                        },
+                    }
+                )
+            )
+        );
         #endregion
 
-        #region Extra Vanilla
-        #region 史莱姆王
+        #region 激光眼 Retinazer
+        buildiincontent.Add(
+            NPCID.Retinazer,
+            new BarInfo(
+                new BarTextures(
+                    NPCID.Retinazer,
+                    new Dictionary<TextureType, BarTexture2D>() 
+                    {
+                        {
+                            TextureType.Frame,
+                            new BarTexture2D(
+                                TextureType.Frame,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossFrame",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla,
+                                (barFillSytle,barFillColot,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; }
+                            )
+                        },
+                        {
+                            TextureType.Head,
+                            new BarTexture2D(
+                                TextureType.Head,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossHead",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                        {
+                            TextureType.Tail,
+                            new BarTexture2D(
+                                TextureType.Tail,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossTail",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                    }
+                )
+            )
+        );
+        #endregion
+        #region 魔焰眼 Spazmatism
+        buildiincontent.Add(
+            NPCID.Spazmatism,
+            new BarInfo(
+                new BarTextures(
+                    NPCID.Spazmatism,
+                    new Dictionary<TextureType, BarTexture2D>()
+                    {
+                        {
+                            TextureType.Frame,
+                            new BarTexture2D(
+                                TextureType.Frame,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossFrame",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla,
+                                (barFillSytle,barFillColot,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; }
+                            )
+                        },
+                        {
+                            TextureType.Head,
+                            new BarTexture2D(
+                                TextureType.Head,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossHead",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                        {
+                            TextureType.Tail,
+                            new BarTexture2D(
+                                TextureType.Tail,ModContent.Request<Texture2D>
+                                (_dvpath + "MechBossTail",AssetRequestMode.ImmediateLoad),
+                                TextureSource.DefaultVanilla
+                            )
+                        },
+                    }
+                )
+            )
+        );
+        #endregion
+        #endregion
+
+        #region 额外原版 Extra Vanilla
+        #region 史莱姆王 King Slime
         buildiincontent.Add(NPCID.KingSlime,
             new BarInfo(
                 new BarTextures(
@@ -110,32 +257,36 @@ internal static class BarData
                         {
                             TextureType.Fill,
                             new BarTexture2D(TextureType.Fill,ModContent.Request<Texture2D>
-                            (_evpath + "KingSlimeFill",
-                            AssetRequestMode.ImmediateLoad),
-                            TextureSource.ExtraVanilla,
-                            (barFillStyles,barFillColor,fillColor,barFrameStyles,extraDrawStyles) => { barFillStyles = BarFillStyles.Extend; })
+                                (_evpath + "KingSlimeFill",
+                                AssetRequestMode.ImmediateLoad),
+                                TextureSource.ExtraVanilla,
+                                (barFillStyles,barFillColor,fillColor,barFrameStyles,extraDrawStyles) => { barFillStyles = BarFillStyles.Extend; }
+                            )
                         },
                         {
                             TextureType.Frame,
                             new BarTexture2D(TextureType.Frame,ModContent.Request<Texture2D>
-                            (_evpath + "KingSlimeFrame",
-                            AssetRequestMode.ImmediateLoad),
-                            TextureSource.ExtraVanilla,
-                            (barFillStyles,barFillColor,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; })
+                                (_evpath + "KingSlimeFrame",
+                                AssetRequestMode.ImmediateLoad),
+                                TextureSource.ExtraVanilla,
+                                (barFillStyles,barFillColor,fillColor,barFrameStyles,extraDrawStyles) => { barFrameStyles = BarFrameStyles.Extend; }
+                            )
                         },
                         {
                             TextureType.Head,
                             new BarTexture2D(TextureType.Head,ModContent.Request<Texture2D>
-                            (_evpath + "KingSlimeHead",
-                            AssetRequestMode.ImmediateLoad),
-                            TextureSource.ExtraVanilla)
+                                (_evpath + "KingSlimeHead",
+                                AssetRequestMode.ImmediateLoad),
+                                TextureSource.ExtraVanilla
+                            )
                         },
                         {
                             TextureType.Tail,
                             new BarTexture2D(TextureType.Tail,ModContent.Request<Texture2D>
-                            (_evpath + "KingSlimeTail",
-                            AssetRequestMode.ImmediateLoad),
-                            TextureSource.ExtraVanilla)
+                                (_evpath + "KingSlimeTail",
+                                AssetRequestMode.ImmediateLoad),
+                                TextureSource.ExtraVanilla
+                            )
                         },
                     }
                 )
